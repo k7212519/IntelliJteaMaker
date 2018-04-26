94876589076792f0b88cc21bb84f52d5b4eca096
k7212519 <791538283@qq.com>
2018-04-27 03:46:23 +0800

    Initial commit

--- app/src/main/java/com/xzw/intellijteamaker/about.java
+++ app/src/main/java/com/xzw/intellijteamaker/about.java
@@ -1,16 +1,24 @@
 package com.xzw.intellijteamaker;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class about extends AppCompatActivity {
     private ImageView imageView_teaMaker;
+    private TextView textView_projectUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        textView_projectUrl = (TextView) findViewById(R.id.address_url);
 
 
 
